66bec376f123da93d431bcdbed1b3257cd6e2a3a
Paul Heading <paulheading@me.com>
2023-07-05 10:19:53 +0100

    better svgs

--- scripts/helpers/get.js
+++ scripts/helpers/get.js
@@ -1,22 +1,8 @@
-// import cache from 'memory-cache'
-
 const get = {}
 
 get.JSON = async function (url) {
-  // const alreadyExists = cache.get(url)
-
-  // if (alreadyExists) {
-  //   console.log('using cache')
-  //   return alreadyExists
-  // }
-
   var data = await fetch(url)
-
-  data = data.json()
-
-  // cache.put(url, data, 60000)
-
-  return data
+  return data.json()
 }
 
 get.gql = async function (url, query, variables = {}) {
