ed2021b5f7b8e424e842f4237d4d5225e6d7a26f
Paul Heading <paulheading@me.com>
2023-07-14 13:59:31 +0100

    hardcode menu

--- components/menu.jsx
+++ components/menu.jsx
@@ -1,14 +1,15 @@
 import styles from 'styles/components/menu.module.scss'
 import { useRouter } from 'next/router'
-import data from 'data/menu'
 import Link from 'next/link'
+import { Fragment } from 'react'
 
 import Desktop from 'logos/paulh/desktop'
 import Mobile from 'logos/paulh/mobile'
 import Wrap from 'components/wrap'
-import Ready from 'components/ready'
 import SmallDown from 'components/media/small/down'
 import SmallUp from 'components/media/small/up'
+import AboutIcon from 'icons/about'
+import ResumeIcon from 'icons/resume'
 
 function Logo() {
   return (
@@ -23,46 +24,52 @@ function Logo() {
   )
 }
 
-function Menu() {
+function CreateLink(props) {
   const { pathname } = useRouter()
-  const isHome = pathname === '/'
+  const { children, icon } = props
+  const linkpath = children.replace(' ', '-').toLowerCase()
+  const href = '/' + linkpath
 
-  const containerStyles = isHome ? styles.home_container : styles.away_container
+  const isActive = href == pathname
 
-  function Content({ title, href, children }) {
-    if (title == 'Home') return
-
-    const isActive = () => href == pathname
-
-    function contentStyles() {
-      var result = styles.link + ' ' + styles[title.toLowerCase() + '_link']
-      if (isActive()) result += ' ' + styles.active
-      return result
-    }
-
-    const props = {
-      className: contentStyles(),
-      href,
-    }
-
-    return isActive() ? <span className={props.className}>{children}</span> : <Link {...props}>{children}</Link>
-  }
-
-  function Links({ title, href, icon }, index) {
-    const props = {
-      title,
-      href,
-      index,
-    }
+  function Content() {
     return (
-      <Ready key={title + index}>
-        <Content {...props}>
-          <SmallUp>{title}</SmallUp>
-          <SmallDown>{icon}</SmallDown>
-        </Content>
-      </Ready>
+      <Fragment>
+        <SmallUp>{children}</SmallUp>
+        <SmallDown>{icon()}</SmallDown>
+      </Fragment>
     )
   }
+
+  function contentStyles() {
+    var custom = linkpath + '_link'
+    var result = styles.link + ' ' + styles[custom]
+
+    if (isActive) result += ' ' + styles.active
+
+    return result
+  }
+
+  const linkProps = {
+    className: contentStyles(),
+    href,
+  }
+
+  return isActive ? (
+    <span className={contentStyles()}>
+      <Content />
+    </span>
+  ) : (
+    <Link {...linkProps}>
+      <Content />
+    </Link>
+  )
+}
+
+function Menu() {
+  const { pathname } = useRouter()
+  const isHome = pathname == '/'
+  const containerStyles = isHome ? styles.home_container : styles.away_container
 
   function WrapLogo() {
     if (isHome) return <Logo />
@@ -83,7 +90,10 @@ function Menu() {
     <div className={containerStyles}>
       <Wrap className={styles.wrap}>
         <WrapLogo />
-        <div className={styles.links}>{data.map(Links)}</div>
+        <div className={styles.links}>
+          <CreateLink icon={AboutIcon}>About</CreateLink>
+          <CreateLink icon={ResumeIcon}>Resume</CreateLink>
+        </div>
       </Wrap>
     </div>
   )
