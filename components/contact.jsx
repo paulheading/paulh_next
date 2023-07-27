e731406a393c250b045b8caa460a117413fb7719
Paul Heading <paulheading@me.com>
2023-07-27 17:50:50 +0100

    braking?

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,5 +1,19 @@
+import styles from 'styles/components/contact.module.scss'
+import { useState } from 'react'
+
+import Window from 'components/window'
+import Form from 'components/form'
+// import Alert from 'components/form/alert'
+import Success from 'components/form/success'
+
 function Contact() {
-  return <div>contact</div>
+  const [success, setSuccess] = useState(false)
+
+  return (
+    <div className={styles.container}>
+      <Window className={styles.window}>{!success ? <Form /> : <Success />}</Window>
+    </div>
+  )
 }
 
 export default Contact
