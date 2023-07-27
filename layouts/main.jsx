3f87c59b624733959aaf9610445acf84d85919f6
Paul Heading <paulheading@me.com>
2023-07-27 10:51:33 +0100

    contact?

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -1,19 +1,19 @@
 import { Fragment } from 'react'
 // import Marquee from 'components/marquee'
 // import Desktop from 'components/desktop'
-import Contact from 'components/contact'
+// import Contact from 'components/contact'
 import Footer from 'components/footer'
-import useCount from 'hooks/useCount'
+// import useCount from 'hooks/useCount'
 import Head from 'components/head'
 
-import layout from 'data/layout.json'
+// import layout from 'data/layout.json'
 
 function Main(props) {
-  var { projects, spotify, gem, npm } = layout
+  // var { projects, spotify, gem, npm } = layout
   var { seo = null } = props
 
-  var heroes = projects.filter(({ hero }) => hero)
-  var count = useCount(heroes)
+  // var heroes = projects.filter(({ hero }) => hero)
+  // var count = useCount(heroes)
   // var hero = heroes[count]
   // var desktopProps = {
   //   projects,
@@ -28,7 +28,7 @@ function Main(props) {
       <Head dynamic={seo} />
       {/* <Marquee hero={hero} /> */}
       {/* <Desktop {...desktopProps} /> */}
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </Fragment>
   )
