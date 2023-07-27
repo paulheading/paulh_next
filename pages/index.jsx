d4ba51f1258220364b4320bb0b7b36a3d6358641
Paul Heading <paulheading@me.com>
2023-07-27 10:24:22 +0100

    remove index layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,23 @@
-// import useCount from 'hooks/useCount'
-// import { Fragment } from 'react'
+import useCount from 'hooks/useCount'
+import { Fragment } from 'react'
 
-// import Hero from 'components/hero'
+import Hero from 'components/hero'
 // import Layout from 'layouts/main'
 
-// import layout from 'data/layout.json'
+import layout from 'data/layout.json'
 
 function Home() {
-  return <div>home</div>
+  var { projects } = layout
+  var heroes = projects.filter(({ hero }) => hero)
+  var count = useCount(heroes)
+  var hero = heroes[count]
 
-  // var { projects } = layout
-  // var heroes = projects.filter(({ hero }) => hero)
-  // var count = useCount(heroes)
-  // var hero = heroes[count]
-
-  // return (
-  //   <Fragment>
-  //     <Hero hero={hero} />
-  //     <Layout />
-  //   </Fragment>
-  // )
+  return (
+    <Fragment>
+      <Hero hero={hero} />
+      {/* <Layout /> */}
+    </Fragment>
+  )
 }
 
 export default Home
