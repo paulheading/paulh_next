9b9dc4b5f72eba9f729f12f888632c27ac1128bf
Paul Heading <paulheading@me.com>
2023-07-27 11:02:53 +0100

    isit contact

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -1,34 +1,34 @@
 import { Fragment } from 'react'
-// import Marquee from 'components/marquee'
-// import Desktop from 'components/desktop'
-import Contact from 'components/contact'
+import Marquee from 'components/marquee'
+import Desktop from 'components/desktop'
+// import Contact from 'components/contact'
 import Footer from 'components/footer'
-// import useCount from 'hooks/useCount'
+import useCount from 'hooks/useCount'
 import Head from 'components/head'
 
-// import layout from 'data/layout.json'
+import layout from 'data/layout.json'
 
 function Main(props) {
-  // var { projects, spotify, gem, npm } = layout
+  var { projects, spotify, gem, npm } = layout
   var { seo = null } = props
 
-  // var heroes = projects.filter(({ hero }) => hero)
-  // var count = useCount(heroes)
-  // var hero = heroes[count]
-  // var desktopProps = {
-  //   projects,
-  //   spotify,
-  //   count,
-  //   gem,
-  //   npm,
-  // }
+  var heroes = projects.filter(({ hero }) => hero)
+  var count = useCount(heroes)
+  var hero = heroes[count]
+  var desktopProps = {
+    projects,
+    spotify,
+    count,
+    gem,
+    npm,
+  }
 
   return (
     <Fragment>
       <Head dynamic={seo} />
-      {/* <Marquee hero={hero} /> */}
-      {/* <Desktop {...desktopProps} /> */}
-      <Contact />
+      <Marquee hero={hero} />
+      <Desktop {...desktopProps} />
+      {/* <Contact /> */}
       <Footer />
     </Fragment>
   )
