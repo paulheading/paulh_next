527a4449cade15f3117208fe1dd1a942ee1cf869
Paul Heading <paulheading@me.com>
2023-07-27 17:54:45 +0100

    form props

--- components/contact.jsx
+++ components/contact.jsx
@@ -9,9 +9,19 @@ import Success from 'components/form/success'
 function Contact() {
   const [success, setSuccess] = useState(false)
 
+  const formProps = {
+    'data-netlify': true,
+    name: 'contact',
+    method: 'POST',
+    id: 'contact',
+    onSubmit: function (e) {
+      e.preventDefault()
+    },
+  }
+
   return (
     <div className={styles.container}>
-      <Window className={styles.window}>{!success ? <Form /> : <Success />}</Window>
+      <Window className={styles.window}>{!success ? <Form {...formProps} /> : <Success />}</Window>
     </div>
   )
 }
