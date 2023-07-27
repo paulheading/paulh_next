bc6485e8c301dd36aa351c24d4c839d8d8c814a5
Paul Heading <paulheading@me.com>
2023-07-27 17:45:59 +0100

    empty contact

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,49 +1,5 @@
-import styles from 'styles/components/contact.module.scss'
-// import { useForm } from 'react-hook-form'
-import { useState } from 'react'
-
-import Window from 'components/window'
-import Form from 'components/form'
-// import Alert from 'components/form/alert'
-import Success from 'components/form/success'
-
-// function onSubmit(event) {
-//   event.preventDefault()
-
-//   var body = [...event.currentTarget.elements]
-
-//   body = body.map(({ name, value }) => encodeURIComponent(name) + '=' + encodeURIComponent(value)).join('&')
-
-//   const method = 'POST'
-//   const headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-
-//   console.log(body)
-
-//   fetch('/', {
-//     method,
-//     headers,
-//     body,
-//   }).catch((error) => alert(error))
-// }
-
 function Contact() {
-  const [success, setSuccess] = useState(false)
-
-  const formProps = {
-    'data-netlify': true,
-    name: 'contact',
-    method: 'POST',
-    id: 'contact',
-    onSubmit: function (e) {
-      e.preventDefault()
-    },
-  }
-
-  return (
-    <div className={styles.container}>
-      <Window className={styles.window}>{!success ? <Form {...formProps} /> : <Success />}</Window>
-    </div>
-  )
+  return <div>contact</div>
 }
 
 export default Contact
