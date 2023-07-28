e3edbec4b0f47bbc00c2d5520a4b518df1029d08
Paul Heading <paulheading@me.com>
2023-07-28 07:59:44 +0100

    working?

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <form name="contact" action="/success" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="yourname">Your Name:</label>
+        <input type="text" name="name" id="yourname" />
+      </p>
+      <p>
+        <label htmlFor="youremail">Your Email: </label> <input type="email" name="email" id="youremail" />
+      </p>
+      <p>
+        <label htmlFor="yourmessage">Message: </label>
+        <textarea name="message" id="yourmessage"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  )
+}
